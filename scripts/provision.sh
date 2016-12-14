9165dae4da979f4b78e20070ff7ca60081671eda
gitsomecode <sjbitcode@gmail.com>
2016-12-14 12:39:10 -0500

    Initial commit

---
+++ scripts/provision.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+
+sudo apt-get -y upgrade
+
+sudo apt-get install -y python3-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y nginx
+sudo apt-get install -y sqlite3
+sudo apt-get install -y tree
+sudo apt-get install -y screen
